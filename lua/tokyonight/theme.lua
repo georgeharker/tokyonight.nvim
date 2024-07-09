57ecccfe42d851c6da4a8f6c3115d047e5260e7a
George Harker <george@george-graphics.co.uk>
2024-07-09 13:55:42 -0700

    ws cleanup

--- lua/tokyonight/theme.lua
+++ lua/tokyonight/theme.lua
@@ -23,7 +23,7 @@ function M.setup(opts)
     local termcol = require("tokyonight.termcol")
     groups = termcol.map_highlights(groups)
   end
-  
+
   for group, hl in pairs(groups) do
     hl = type(hl) == "string" and { link = hl } or hl
     vim.api.nvim_set_hl(0, group, hl)
